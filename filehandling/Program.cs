263faf84281333a53903b3e19f1b57321ae23d11
Shilpa.Devanga <Shilpa.Devanga@INLPF2KZ387>
2022-09-26 19:58:05 +0530

    Add project files.

---
+++ filehandling/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace filehandling
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            FileStream fs = new FileStream("C:\\Users\\shilpa.devanga\\Documents\\testing.txt", FileMode.OpenOrCreate) ;
+            StreamWriter streamWriter = new StreamWriter(fs) ;
+            streamWriter.WriteLine("hii!, this is the first line of the code writing inside the file..!");
+            streamWriter.Close();
+            fs.Close();
+        }
+    }
+}
---
+++ filehandling/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("filehandling")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Entain PLC")]
+[assembly: AssemblyProduct("filehandling")]
+[assembly: AssemblyCopyright("Copyright © Entain PLC 2022")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("9f55f5bd-6811-4d74-96d7-ff3ce6c3109c")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
